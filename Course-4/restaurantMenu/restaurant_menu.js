238cd1efee8bb784a3f4b976d0eaceff8cc4651b
Pratikchandrathakur <krisprogrammer1@gmail.com>
2024-09-29 05:02:17 -0400

    adding desert  menu for restaurant menu

--- Course-4/restaurantMenu/restaurant_menu.js
+++ Course-4/restaurantMenu/restaurant_menu.js
@@ -9,3 +9,8 @@ const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
         mainCourseMenu.forEach((item, index) => {
         mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
         document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+        let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
