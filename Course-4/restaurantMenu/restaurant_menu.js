ac7d9a14dff418ee32be0f696156f2b40732c4ae
Pratikchandrathakur <krisprogrammer1@gmail.com>
2024-09-29 04:59:37 -0400

    adding main course menu for restaurant menu

--- Course-4/restaurantMenu/restaurant_menu.js
+++ Course-4/restaurantMenu/restaurant_menu.js
@@ -4,3 +4,8 @@ const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 
  const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+        let mainCourseItem = '';
+        mainCourseMenu.forEach((item, index) => {
+        mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+        document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
